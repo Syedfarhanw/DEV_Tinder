d820fd3a672128e6319d7d43c0ea5e1063ff2ab3
SyedFarhan <farhanaktherw@gmail.com>
2026-01-15 01:10:36 +0530

    Playing with the routes

--- src/app.js
+++ src/app.js
@@ -3,12 +3,35 @@ require("dotenv").config();
 
 const app = express();
 
-app.use("/test",(req, res) => {
-    res.send("Hello")
+app.get("/user", (req, res) => {
+    res.send({firstName: "Syed", lastName: "Farhan"})
+});
+
+app.post("/user/:userId", (req, res) => {
+    console.log(req.params);
+    res.send({firstName: "Syed", lastName: "Farhan"})
 })
 
-app
+app.post("/user", (req, res) => {
+    res.send("Data saved Successfully")
+})
 
+app.use("/hello/2",(req, res) => {
+    res.send("Hellooooo")
+});;
+
+app.use("/hello",(req, res) => {
+    res.send("Hello2")
+})
+
+app.use("/test",(req, res) => {
+    res.send("Hello Test")
+});
+
+
+app.use("/",(req, res) => {
+    res.send("BOSS")
+});
 const port = process.env.PORT || 5000
 
 const server = app.listen(port, () => {
